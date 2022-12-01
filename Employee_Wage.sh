a0ef93cdb2fb203f77c37beba3832a078f3d8c9e
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-01 15:47:59 +0530

    Daily Wage Of Employee

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -1,10 +1,14 @@
 echo "---------WelCome to Employee Wage Computation----------------------"
  isPresent=$(($RANDOM%2))
-
+ WagePerHour=20;
+ FullDayHour=8
   if [ $isPresent -eq 1 ]
   then 
   	echo "Employee is Present "
+  	DailyEmployeeWage=$(($WagePerHour * $FullDayHour));
+	echo $DailyEmployeeWage;
   else 
   	echo "Employee is Absent "
 
   fi
+  
