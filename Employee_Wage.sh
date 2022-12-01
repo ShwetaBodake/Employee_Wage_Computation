2e363a4c69d639d34e2161b051f945e35de405fe
ShwetaBodake <shwetabodake8108@gmail.com>
2022-12-01 16:05:40 +0530

    PartTime_Employee_Wage

--- Employee_Wage.sh
+++ Employee_Wage.sh
@@ -1,12 +1,20 @@
 echo "---------WelCome to Employee Wage Computation----------------------"
- isPresent=$(($RANDOM%2))
+ isPresent=$(($RANDOM%3))
  WagePerHour=20;
- FullDayHour=8
+ FullDayHour=8;
+ PartTimeHour=4;
+
   if [ $isPresent -eq 1 ]
   then 
   	echo "Employee is Present "
   	DailyEmployeeWage=$(($WagePerHour * $FullDayHour));
 	echo $DailyEmployeeWage;
+elif [ $isPresent -eq 2 ]
+ then
+ 	echo "Employee is PartTime Present"
+ 	DailyPartTimeWage=$(($PartTimeHour * $WagePerHour));
+ 	echo "PartTime Employee Wage =$DailyPartTimeWage"
+	#statements
   else 
   	echo "Employee is Absent "
 
